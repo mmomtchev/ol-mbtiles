16c0fceffa0554d7038c5de80d4aecd430088856
Momtchil Momtchev <momtchil@momtchev.com>
2023-03-02 19:29:57 +0100

    more extensive unit testing

--- test/source.test.ts
+++ test/source.test.ts
@@ -11,28 +11,25 @@ const assert = chai.assert;
 const expect = chai.expect;
 
 describe('MBTilesSource', () => {
+  const tileCoord = [12, 2118, 1461];
+  const extent = [684875.7734351791, 5733388.6176145, 694659.7130556817, 5743172.5572350025];
+  const resolution = 19.109257071294063;
+  const projection = 'ESPG:3587';
+
   it('should retrieve tiles', (done) => {
-    const layers = ['mountain_peak', 'transportation'];
     const source = new MBTilesSource({
       url: 'https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles',
-      layers,
-      attributions: ['MapTiler', 'OSM', 'https://data.maptiler.com/downloads/dataset/osm/europe/'],
-      maxZoom: 14,
-      minZoom: 0,
       sqlWorkers: 1
     });
-    const extent = [684875.7734351791, 5733388.6176145, 694659.7130556817, 5743172.5572350025];
-    const resolution = 19.109257071294063;
-    const projection = 'ESPG:3587';
     const loadFn = source.getTileLoadFunction();
-    const tile = new VectorTile([12, 2118, 1461], TileState.EMPTY, '', new MBTilesFormat({ layers }), loadFn);
+    const tile = new VectorTile(tileCoord, TileState.EMPTY, '', new MBTilesFormat(), loadFn);
 
     tile.setLoader = chai.spy((loader: FeatureLoader) => {
       expect(loader).is.a('function');
 
       tile.setFeatures = chai.spy((features: Feature[]) => {
         try {
-          assert.lengthOf(features, 71);
+          assert.lengthOf(features, 278);
           const rdb = features.find((f) =>
             f.getProperties()['name'] === 'Roc des Boeufs') as unknown as RenderFeature;
           assert.instanceOf(rdb, RenderFeature);
@@ -50,6 +47,98 @@ describe('MBTilesSource', () => {
         try {
           expect(tile.setFeatures).to.be.called.once;
           expect(tile.onLoad).to.be.called.once;
+          expect(tile.onError).to.be.not.be.called;
+          source.destroy();
+          done();
+          return;
+        } catch (e) {
+          source.destroy();
+          done(e);
+        }
+      });
+
+      loader.call(source, extent, resolution, projection, [], () => undefined);
+    });
+
+    loadFn(tile, '');
+    expect(tile.setLoader).to.be.called.once;
+  });
+
+  it('should filter by layer', (done) => {
+    const layers = ['mountain_peak', 'transportation'];
+    const source = new MBTilesSource({
+      url: 'https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles',
+      layers,
+      maxZoom: 14,
+      minZoom: 0,
+      sqlWorkers: 1
+    });
+    const loadFn = source.getTileLoadFunction();
+    const tile = new VectorTile(tileCoord, TileState.EMPTY, '', new MBTilesFormat({layers}), loadFn);
+
+    tile.setLoader = chai.spy((loader: FeatureLoader) => {
+      expect(loader).is.a('function');
+
+      tile.setFeatures = chai.spy((features: Feature[]) => {
+        try {
+          assert.lengthOf(features, 71);
+          const rdb = features.find((f) =>
+            f.getProperties()['name'] === 'Roc des Boeufs') as unknown as RenderFeature;
+          assert.instanceOf(rdb, RenderFeature);
+          assert.strictEqual(rdb?.getProperties()['ele'], 1774);
+          assert.strictEqual(rdb?.getGeometry()?.getType(), 'Point');
+          assert.strictEqual(rdb?.getProperties()['layer'], 'mountain_peak');
+          assert.deepEqual(rdb.getExtent(), [686421.23460082, 5741182.805842454, 686421.23460082, 5741182.805842454]);
+        } catch (e) {
+          source.destroy();
+          done(e);
+        }
+      });
+
+      tile.onError = chai.spy();
+      tile.onLoad = chai.spy(() => {
+        try {
+          expect(tile.setFeatures).to.be.called.once;
+          expect(tile.onLoad).to.be.called.once;
+          expect(tile.onError).to.be.not.be.called;
+          source.destroy();
+          done();
+          return;
+        } catch (e) {
+          source.destroy();
+          done(e);
+        }
+      });
+
+      loader.call(source, extent, resolution, projection, [], () => undefined);
+    });
+
+    loadFn(tile, '');
+    expect(tile.setLoader).to.be.called.once;
+  });
+
+  it('should report errors', (done) => {
+    const layers = ['mountain_peak', 'transportation'];
+    const source = new MBTilesSource({
+      url: 'https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles',
+      layers,
+      maxZoom: 14,
+      minZoom: 0,
+      sqlWorkers: 1
+    });
+    const loadFn = source.getTileLoadFunction();
+    const tile = new VectorTile([1, 100, 100], TileState.EMPTY, '', new MBTilesFormat({ layers }), loadFn);
+
+    tile.setLoader = chai.spy((loader: FeatureLoader) => {
+      expect(loader).is.a('function');
+
+      tile.setFeatures = chai.spy();
+      tile.onLoad = chai.spy();
+      tile.onError = chai.spy(() => {
+        try {
+          expect(tile.setFeatures).to.not.be.called;
+          expect(tile.onLoad).to.be.not.be.called;
+          expect(tile.onError).to.be.be.called.once;
           source.destroy();
           done();
           return;
